ef903436a6dfdda12a4e848a2894f7e596d1f25c
Viktoria Tibenska <vtibensk8@gmail.com>
2018-09-25 22:19:19 +0200

    01-text century functionality

--- 01-text/stat.py
+++ 01-text/stat.py
@@ -17,11 +17,41 @@ if command == 'composer':
         clean = re.sub( r"\(.*\)", '', m.group(1) )
         clean = clean.split(';')
         clean = map(str.strip, clean)
-        #clean = clean.strip()
         clean = list(filter(None, clean))
         composers.extend(clean)
 
     for composer, count in Counter(composers).most_common():
         print(composer + ': ' + str(count))
 elif command == 'century':
-    print('century')
+    centuries = []
+    for line in f:
+        r = re.compile( r"Composition Year: (.*)" )
+        m = r.match(line)
+
+        if m is None or m.group(1) == '': continue
+
+        clean = m.group(1).strip()
+        
+        # handle centuries specified as XXth century
+        centuryRegexMatch = re.compile( r"[0-9]{2}th century" ).match(clean)
+        if centuryRegexMatch is not None:
+            centuries.append(int(centuryRegexMatch.group()[:2]))
+            continue
+
+        # handle dates in format nn. nn. nnnn
+        datesRegexMatch = re.compile( r"[0-9]{1,2}. [0-9]{1,2}. [0-9]{4}" ).match(clean)
+        if datesRegexMatch is not None:
+            clean = datesRegexMatch.group()[-4:]
+
+        # handle ranges such as 0000--0000
+        rangeRegexMatch = re.compile( r"[0-9]{4}--[0-9]{4}" ).match(clean)
+        if rangeRegexMatch is not None:
+            clean = rangeRegexMatch.group()[:4]
+
+        # remove all non digit characters
+        clean = re.sub(r"\D", '', clean)
+        if clean != '':
+            centuries.append(int(clean[:2])+1)
+
+    for century, count in Counter(centuries).most_common():
+        print(str(century) + 'th century: ' + str(count))
